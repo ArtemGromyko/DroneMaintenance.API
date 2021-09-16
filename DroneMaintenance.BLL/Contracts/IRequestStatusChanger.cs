c96aac8d80f2835e54f2ebd994a6025cf0be815d
ArtemGromyko <user@DESKTOP-7V56IJ5>
2021-09-16 18:26:07 +0300

    add request status changer

---
+++ DroneMaintenance.BLL/Contracts/IRequestStatusChanger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneMaintenance.BLL.Contracts
+{
+    public interface IRequestStatusChanger
+    {
+        Task UpdateRequestStatusAsync();
+    }
+}
---
+++ DroneMaintenance.BLL/Services/RequestStatusChanger.cs
@@ -0,0 +1,27 @@
+﻿using DroneMaintenance.BLL.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DroneMaintenance.BLL.Services
+{
+    public class RequestStatusChanger : IRequestStatusChanger
+    {
+        private readonly IServiceProvider _sp;
+
+        public RequestStatusChanger(IServiceProvider sp)
+        {
+            _sp = sp;
+        }
+
+        public async Task UpdateRequestStatusAsync()
+        {
+            using var scope = _sp.CreateScope();
+            using(var httpClient = new HttpClient())
+            {
+
+            }
+        }
+    }
+}
