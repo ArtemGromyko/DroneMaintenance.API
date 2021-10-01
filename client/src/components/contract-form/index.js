e009d8ea00dc697bdf747579738bf4aa81294672
ArtemGromyko <user@DESKTOP-7V56IJ5>
2021-10-01 17:57:43 +0300

    refactor contract form

--- client/src/components/contract-form/index.js
+++ client/src/components/contract-form/index.js
@@ -9,6 +9,10 @@ import { MainContext } from '../../contexts/main-context';
 import { modelContext } from '../../contexts/models-context';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { getParts } from '../../services/api-service/parts-service';
+import HttpError from './../../errors/HttpError';
+import Error from '../error';
+import { useHistory } from 'react-router-dom';
+import Spinner from '../spinner';
 
 const useStyles = makeStyles(() => ({
     root: {
@@ -30,53 +34,111 @@ const useStyles = makeStyles(() => ({
 }));
 
 const ContractForm = () => {
+    const [part, setPart] = useState('');
+    const [quantity, setQuantity] = useState('');
     const [parts, setParts] = useState([]);
-    const classes = useStyles();
+    const [isDisabled, setDisabled] = useState(true);
+    const [error, setError] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
 
     const { user } = useContext(MainContext);
     const { model } = useContext(modelContext);
 
+    const classes = useStyles();
+    const history = useHistory();
+
     useEffect(() => {
         if (user) {
+            console.log('model');
+            console.log(model);
             getParts(user.token).then((res) => {
                 setParts(res);
+                setIsLoading(false);
+            }).catch((error) => {
+                if (error instanceof HttpError) {
+                    if (error.code === 401) {
+                        history.push('/login');
+                    } else {
+                        setError(error);
+                    }
+                }
             })
-            console.log(parts);
         } else {
             setParts([]);
         }
-        console.log(model);
     }, [user]);
 
+    useEffect(() => {
+        if (part === '' || quantity === '') {
+            setDisabled(true);
+        } else {
+            setDisabled(false);
+        }
+    }, [part, quantity]);
+
+    function handleChange(event) {
+        switch (event.target.name) {
+            case 'spare-part':
+                console.log('/spare-part');
+                console.log(event.target.value);
+                console.log(typeof event.target.value);
+                setPart(event.target.value);
+                break;
+            case 'quantity':
+                console.log('quantity');
+                console.log(event.target.value);
+                console.log(typeof event.target.value);
+                setQuantity(event.target.value);
+                break;
+            case 'spare-part-list':
+                console.log('/spare-part-list');
+                console.log(event.target.value);
+                console.log(typeof event.target.value);
+                setPart(event.target.value);
+                break;
+        }
+    }
+
     return (
-            <Grid className={classes.root} container justifyContent='center' alignItems='center'>
-                <Paper className={classes.paperStyle} variant="outlined">
-                    <Grid align='center'>
-                        <h2>Add spare parts</h2>
+        <>
+            {isLoading ? <Spinner /> :
+                error ? <Error message={error.message} code={error.code} /> : (
+                    <Grid className={classes.root} container justifyContent='center' alignItems='center'>
+                        <Paper className={classes.paperStyle} variant="outlined">
+                            <Grid align='center'>
+                                <h2>Add spare parts</h2>
+                            </Grid>
+                            <Autocomplete
+                                options={parts}
+                                getOptionLabel={(option) => option.name}
+                                renderInput={(params) =>
+                                    <TextField
+                                        name="spare-part" onChange={handleChange}
+                                        required {...params} label="Spare part" variant="outlined" />}
+                            />
+                            <TextField name='quantity' label='Quantity'
+                                className={classes.textField}
+                                type='number'
+                                placeholder='Enter quantity'
+                                required
+                                onChange={handleChange}
+                                min={0}
+                                fullWidth variant="outlined" />
+                            <Button className={classes.buttonStyle} type='submit' color='primary'
+                                disabled={isDisabled}
+                                variant='contained' fullWidth>
+                                Add
+                            </Button>
+                            <Typography align='right' style={{ marginTop: '6rem' }}>
+                                <Link style={{ textDecoration: 'none' }} to="/requests">
+                                    Cancel
+                                </Link>
+                            </Typography>
+                        </Paper>
                     </Grid>
-                    <Autocomplete
-                        id="spare-part"
-                        options={parts}
-                        getOptionLabel={(option) => option.name}
-                        renderInput={(params) => <TextField {...params} label="Spare part" variant="outlined" />}
-                    />
-                    <TextField name='quantity' label='Quantity'
-                        className={classes.textField}
-                        type='number'
-                        placeholder='Enter quantity'
-                        min={0}
-                        fullWidth variant="outlined" />
-                    <Button className={classes.buttonStyle} type='submit' color='primary'
-                        variant='contained' fullWidth>
-                            Add
-                    </Button>
-                    <Typography align='right' style={{ marginTop: '6rem' }}>
-                        <Link style={{ textDecoration: 'none' }} to="/requests">
-                            Cancel
-                        </Link>
-                    </Typography>
-                </Paper>
-            </Grid>
+                )
+            }
+        </>
     );
 }
 
